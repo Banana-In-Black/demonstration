2ef7662aab7f4d0171f335304c9858d325ab7837
Banana-In-Black <banana.in.black@gmail.com>
2013-08-18 18:20:53 +0800

    More AngularJS!

--- js/main.js
+++ js/main.js
@@ -1,42 +1,5 @@
 "use strict";
 
-$(function() {
-    
-    /* SideMenu collapse and expand function */
-    
-    var fadeDuration = 200;
-    var $expandIcon = 
-        $('.icon-step-forward')
-            .click(function() {
-                $('.side-menu')
-                    .toggleClass('side-menu-reserve')
-                    .children(':not(.icon-white)').show();
-                $('.context').removeClass('span11').addClass('span9');
-                $(this).hide();
-            });
-    var $collapseIcon = 
-        $('.icon-step-backward')
-            .click(function(){
-                $('.side-menu')
-                    .toggleClass('side-menu-reserve')
-                    .children(':not(.icon-white)').hide();
-                $('.context').removeClass('span9').addClass('span11');
-                $(this).hide();
-                $expandIcon.fadeIn(fadeDuration);
-            });
-    
-    $('.side-menu').hover(function() {
-        if(!$('.side-menu').is('.side-menu-reserve')) {
-            $collapseIcon.fadeIn(fadeDuration);
-        }
-    }, function() {
-        if(!$('.side-menu').is('.side-menu-reserve')) {
-            $collapseIcon.fadeOut(fadeDuration);
-        }
-    });
-    
-});
-
 /**
  * @param target a jQuery selector string, target DOM; or scrollTop value
  * @param container a jQuery selector string, represents DOM which contains target, default is 'html, body'
